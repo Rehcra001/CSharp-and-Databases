7502022e15b6cb74bd8233652865c459e6c3ddec
Rehcra001 <tkropf12345@gmail.com>
2023-09-01 09:43:09 +0200

    Add command to navigate to the publisher view

--- CSharp and Database/BooksDB WPF MVVM/ViewModels/MainViewModel.cs
+++ CSharp and Database/BooksDB WPF MVVM/ViewModels/MainViewModel.cs
@@ -11,37 +11,51 @@ namespace BooksDB_WPF_MVVM.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
-		private INavigationService _navigation;
+        private INavigationService _navigation;
 
-		public INavigationService Navigation
-		{
-			get { return _navigation; }
-			set 
-			{ 
-				_navigation = value;
-				OnPropertyChanged();
-			}
-		}
+        public INavigationService Navigation
+        {
+            get { return _navigation; }
+            set
+            {
+                _navigation = value;
+                OnPropertyChanged();
+            }
+        }
 
-		private ISharedDataService _sharedData;
+        private ISharedDataService _sharedData;
 
-		public ISharedDataService SharedData
-		{
-			get { return _sharedData; }
-			set 
-			{ 
-				_sharedData = value;
-				OnPropertyChanged();
-			}
-		}
+        public ISharedDataService SharedData
+        {
+            get { return _sharedData; }
+            set
+            {
+                _sharedData = value;
+                OnPropertyChanged();
+            }
+        }
 
 
-		public ICommand	 NavigateToTitleViewCommand { get; set; }
+        public ICommand NavigateToTitleViewCommand { get; set; }
+        public ICommand NavigateToPublisherViewCommand { get; set; }
         public MainViewModel(INavigationService navigation, ISharedDataService sharedDataService)
         {
-			Navigation = navigation;
-			NavigateToTitleViewCommand = new RelayCommand(NavigateToTitleView, CanNavigateToTitleView);
-			SharedData = sharedDataService;
+            Navigation = navigation;
+
+            NavigateToTitleViewCommand = new RelayCommand(NavigateToTitleView, CanNavigateToTitleView);
+            NavigateToPublisherViewCommand = new RelayCommand(NavigateToPublisherView, CanNavigateToPulisherView);
+
+            SharedData = sharedDataService;
+        }
+
+        private bool CanNavigateToPulisherView(object obj)
+        {
+            return true;
+        }
+
+        private void NavigateToPublisherView(object obj)
+        {
+            Navigation.NavigateTo<PublisherViewModel>();
         }
 
         private bool CanNavigateToTitleView(object obj)
